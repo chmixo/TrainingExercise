52fb528fe140e0de37d8ffef3746d394c2b7de50
chmixo <simohamed.chami@gmail.com>
2021-06-07 17:34:12 -0400

    changement du port serveur pour 8888 et debug de qlqs annotations spring

--- src/main/java/com/training/exercise/ExerciseApplication.java
+++ src/main/java/com/training/exercise/ExerciseApplication.java
@@ -2,14 +2,19 @@ package com.training.exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
 public class ExerciseApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(ExerciseApplication.class, args);
-		System.out.println("Hello World");
 	}
 
 }
