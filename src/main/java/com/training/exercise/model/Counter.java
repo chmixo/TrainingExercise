5cbf591070483790e71c0754eb17fdcb2a80ae98
chmixo <simohamed.chami@gmail.com>
2021-06-09 17:50:48 -0400

    ajout du modele Counter

---
+++ src/main/java/com/training/exercise/model/Counter.java
@@ -0,0 +1,39 @@
+package com.training.exercise.model;
+
+public class Counter {
+
+    private String name;
+    private int value;
+
+    public Counter() {
+    }
+
+    public Counter(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Counter{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
--- src/main/java/com/training/exercise/web/controller/CounterController.java
+++ src/main/java/com/training/exercise/web/controller/CounterController.java
@@ -1,4 +1,18 @@
 package com.training.exercise.web.controller;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class CounterController {
+
+    @GetMapping(value = "/counters")
+    public String getCounters(){
+        return "This is a counter test";
+    }
+
+    @GetMapping(value = "/counters/{counter}")
+    public String getCounter(@PathVariable String counter){
+        return ("You asked for counter " + counter);
+    }
+
+
 }
