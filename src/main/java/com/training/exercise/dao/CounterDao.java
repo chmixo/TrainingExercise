fcc8aa447abbd0e396d453e032e1be8b91d7d709
chmixo <simohamed.chami@gmail.com>
2021-06-09 18:50:52 -0400

    Ajout du package dao avec interface CounterDao et ajout de son implementation dans CounterDaoI

--- src/main/java/com/training/exercise/dao/CounterDao.java
+++ src/main/java/com/training/exercise/dao/CounterDao.java
@@ -1,2 +1,12 @@
-package com.training.exercise.dao;public interface CounterDAO {
+package com.training.exercise.dao;
+
+import com.training.exercise.model.Counter;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface CounterDao {
+        public HashMap<String, Integer> findAll();
+        public Counter findById(String id);
+        public Counter save(Counter counter);
 }
--- src/main/java/com/training/exercise/dao/CounterDaoI.java
+++ src/main/java/com/training/exercise/dao/CounterDaoI.java
@@ -1,2 +1,42 @@
-package com.training.exercise.dao;public class CounterDaoI {
+package com.training.exercise.dao;
+
+import com.training.exercise.model.Counter;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class CounterDaoI implements CounterDao {
+
+    public static HashMap<String, Integer> counters = new HashMap<>();
+    static {
+        counters.put("abc", 5);
+        counters.put("xyz", 3);
+    }
+
+    @Override
+    public HashMap<String, Integer> findAll() {
+        return counters;
+    }
+
+    @Override
+    public Counter findById(String id) {
+        // We iterate through the HashMap to find if the specified Id exists
+        Iterator it = counters.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry counter = (Map.Entry)it.next();
+            if(counter.getKey().equals(id)){
+                return new Counter((String) counter.getKey(), (int) counter.getValue());
+            }
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+        return null;
+    }
+
+    @Override
+    public Counter save(Counter counter) {
+        counters.put(counter.getName(), counter.getValue());
+        return counter;
+    }
 }
